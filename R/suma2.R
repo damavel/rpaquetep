1330517720e4ba8bf281f29b0ce26c29fb07afa8
Juan Camilo Diaz Zapata <jucdiazza@unal.edu.co>
2016-12-03 15:08:37 -0500

    first commit

---
+++ R/suma2.R
@@ -0,0 +1,15 @@
+#' Suma dos numeros
+#'
+#' Esta función suma dos numeros
+#'
+#' @param a primer numero
+#' @param b segundo numero
+#' @export
+#' @examples
+#' suma2(a=3,b=4)
+#' suma2(a=5,b=6)
+
+suma2<-function(a,b){
+  r<-a+b
+  r
+}
