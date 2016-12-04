fc100b90d301d8b35e4b140bd8bffeb0046e2e64
jucdiaz <jucdiazza@unal.edu.co>
2016-12-03 23:08:49 -0500

    update

---
+++ R/multi2.R
@@ -0,0 +1,15 @@
+#' Multiplica dos numeros
+#'
+#' esta funcion multiplica dos numeros
+#'
+#' @param a primer numero
+#' @param b segundo numero
+#' @export
+#' @examples
+#' multi2(a=2,b=4)
+#' multi2(a=3,b=5)
+
+multi2<-function(a,b){
+  r<-a*b
+  r
+}
